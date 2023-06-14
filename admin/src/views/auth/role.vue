7a29f95e953b0d331287bc7698e52bc63ef08fe5
chunshan <1484082125@qq.com>
2023-06-14 15:07:03 +0800

    封装

--- admin/src/views/auth/role.vue
+++ admin/src/views/auth/role.vue
@@ -5,6 +5,10 @@
     <el-row>
       <el-col :span="4">
         <el-card shadow="never" class="search-wrapper">
+          <el-space>
+            <el-text size="large">角色</el-text>
+            <el-button type="text">添加</el-button>
+          </el-space>
           <el-scrollbar height="600px">
             <el-menu>
               <el-menu-item index="2">
@@ -16,7 +20,6 @@
       </el-col>
       <el-col :span="20" style="padding-left: 8px;box-sizing: border-box;">
         <el-card shadow="never" class="search-wrapper">
-          
         </el-card>
       </el-col>
     </el-row>
