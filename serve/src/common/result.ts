639aea4e2ab2f45cc609857c9ca21ca5d4e33d91
chunshan <1484082125@qq.com>
2023-07-13 08:51:20 +0800

    类型定义

--- serve/src/common/result.ts
+++ serve/src/common/result.ts
@@ -5,12 +5,15 @@ export class ResultData<T> {
         this.message = message || 'ok'
         this.data = data || null
     }
-    @ApiProperty({ type: 'number', default: 200 })
+    @ApiProperty({ type: 'number', default: 200, description: "状态" })
     code: number
 
-    @ApiProperty({ type: 'string', default: 'ok' })
+    @ApiProperty({ type: 'string', default: 'ok', description: "消息" })
     message: string
 
-    @ApiProperty({ type: 'any', default: null })
+    @ApiProperty({ type: 'any', default: null, description: "数据" })
     data: T
+
+    @ApiProperty({ type: 'boolean', default: false, description: "是否成功" })
+    success: boolean
 }
