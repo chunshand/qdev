b1c71df5433e971144fd3031b1fc7f40678307e3
chunshan <1484082125@qq.com>
2023-07-14 13:46:55 +0800

    1

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,30 +2,33 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "QDev",
-  description: "快速开发管理系统",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: '首页', link: '/' },
-      { text: '快速开发', link: '/quick-dev' },
-      { text: '学习资料', link: '/quick-dev'  },
-      { text: '感谢', link: '/quick-dev'  }
-    ],
-    sidebar: {
-      "/quick-dev": [
-        {
-          text: '快速开发',
-          items: []
+    title: "QDev",
+    description: "快速开发管理系统",
+    themeConfig: {
+        search: {
+            provider: 'local'
+        },
+        // https://vitepress.dev/reference/default-theme-config
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '快速开发', link: '/quick-dev' },
+            { text: '学习资料', link: '/quick-dev' },
+            { text: '感谢', link: '/quick-dev' }
+        ],
+        sidebar: {
+            "/quick-dev": [
+                {
+                    text: '快速开发',
+                    items: []
+                }
+            ]
+        },
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/chunshand' }
+        ],
+        footer: {
+            message: 'Released under the MIT License.',
+            copyright: 'Copyright © 2023-present chunshand'
         }
-      ]
-    },
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/chunshand' }
-    ],
-    footer: {
-      message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2023-present chunshand'
     }
-  }
 })
--- docs/index.md
+++ docs/index.md
@@ -22,18 +22,18 @@ features:
   - title: 前后端分离
     details: 前端使用Vue3、Element plus，后端使用Nestjs、TypeOrm
   - title: 基础功能
-    details: 登录、权限管理(含菜单)、角色管理、管理员管理、会员管理、系统配置（开发中）、公告（开发中）
-  - title: 微信生态功能（开发中）
+    details: 登录、权限管理(含菜单)、角色管理、管理员管理、会员管理、系统配置（开发中）
+  - title: 微信生态功能（暂无）
     details: 微信公众号、微信小程序、企业微信
-  - title: 存储功能（开发中）
-    details: 本地存储、阿里云对象存储、腾讯云对象存储、阿里云视频点播
-  - title: 三方登录（开发中）
+  - title: 存储功能
+    details: 本地存储，阿里云对象存储、腾讯云对象存储、阿里云视频点播、直播（暂无）
+  - title: 三方登录（暂无）
     details: 微信、腾讯、微博、钉钉等
-  - title: 定时任务（开发中）
+  - title: 定时任务（暂无）
     details: 动态添加、定时备份、定时删除等
-  - title: 支付功能（开发中）
+  - title: 支付功能（暂无）
     details: 微信、支付宝等
-  - title: 其他功能（开发中）
+  - title: 其他功能（暂无）
     details: 文章、博客、商品等
 ---
 
