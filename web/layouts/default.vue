8b09e425d9872302fe4f4b9bdb969d7052df161e
chunshan <1484082125@qq.com>
2023-07-17 14:45:27 +0800

    开始进行web的编写

---
+++ web/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <slot />
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped></style>
---
+++ web/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    首页
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped></style>
